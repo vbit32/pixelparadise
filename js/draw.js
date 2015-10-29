3afb947b83212300bf0a1343edd35bef0f9bafd7
vbit32 <valerie.bay2@gmail.com>
2015-10-29 18:54:31 -0400

    start working on generating color scheme for sky

--- js/draw.js
+++ js/draw.js
@@ -2,40 +2,68 @@
 
 var maxRows = 30 ;
 
-for (var i = 0 ; i < maxRows ; i ++)
+//create array that will hold colors
+var bluesPurples = ['#7A8097','#977A80','#99A0BD','#BD99A0','#977A80'] ;
+var redsOrangesYellows = ['#ff9b9b','#f0b186','#ece5a7','#e39277','#e7ae74'] ;
+
+//actual array to use
+//holds the primary colors to base pattern off of
+//pass in the first color, keep passing it in with varying luminosities
+//then pass in next color
+
+var skyColors = ['#6666FF','#CC99FF', '#FFCCCC', '#FFCC99', '#FF9999'] ;
+
+for (var i = 0 ; i < skyColors.length ; i ++)
 {
-	createAndAppend () ;
+	var color = skyColors[i];
+	for (var j = 0 ; j < 3 ; j ++)
+	{
+		var newShade = colorLuminance(color,0.15) ;
+		createAndAppend(newShade);
+		color = newShade;
+	}
 }
 
-
-function createAndAppend ()
+function createAndAppend (color)
 {
+	//will create a div with those css dimensions
+	//then will set the color to the hex code that was passed in
+	//
 	var div = document.createElement("div");
 	console.log(document);
 	div.className = 'div1';
+	div.style.background = color;
 	//append the div
 	document.body.appendChild(div);
 	//love the div
 }
 
-//overload function
-function createAndAppend (cssClass)
-{
-	var div = document.createElement("div");
-	console.log(document);
-	div.className = cssClass;
-	//append the div
-	document.body.appendChild(div);
-	//love the div
+function colorLuminance(hex, lum) {
+
+	// validate hex string
+	hex = String(hex).replace(/[^0-9a-f]/gi, '');
+	if (hex.length < 6) {
+		hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
+	}
+	lum = lum || 0;
+
+	// convert to decimal and change luminosity
+	var rgb = "#", c, i;
+	for (i = 0; i < 3; i++) {
+		c = parseInt(hex.substr(i*2,2), 16);
+		c = Math.round(Math.min(Math.max(0, c + (c * lum)), 255)).toString(16);
+		rgb += ("00"+c).substr(c.length);
+	}
+
+	return rgb;
 }
 
-function drawClouds() {
-
+function drawClouds(idName) {
 	//create div 
-	var canvas = document.getElementById('myCanvas');
+	var canvas = document.getElementById(idName);
 	var context = canvas.getContext('2d');
 
-	  // begin custom shape
+	//begin custom shape
 	context.beginPath();
 	context.moveTo(170, 80);
 	context.bezierCurveTo(130, 100, 130, 150, 230, 150);
@@ -45,7 +73,8 @@ function drawClouds() {
 	context.bezierCurveTo(320, 5, 250, 20, 250, 50);
 	context.bezierCurveTo(200, 5, 150, 20, 170, 80);
 
-	  // complete custom shape
+	//complete custom shape
+	//later remove js styles, add css style class to canvas element in html
 	context.closePath();
 	context.lineWidth = 5;
 	context.strokeStyle = 'blue';
